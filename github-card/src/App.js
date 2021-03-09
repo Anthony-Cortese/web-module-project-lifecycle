6d877ae2e5ea0829f9d5ff8bed15c2e1f6d43940
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-03-09 15:39:51 -0700

    completed the project

---
+++ github-card/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import Card from './components/Card'
+
+class App extends Component {
+  state = {
+    userData: {},
+    followers: []
+  }
+//method inside of an object
+//axios call
+  componentDidMount() {
+    axios
+    .get('https://api.github.com/users/Anthony-Cortese')
+    .then(res => {
+      this.setState({userData: res.data})
+      console.log(res.data)
+      axios.get(res.data.followers_url)
+      //followers_url: "https://api.github.com/users/Anthony-Cortese/followers"
+      .then(res => {
+        this.setState({followers: res.data})
+      })
+      .catch(err=> console.log(err.message))
+    })
+    .catch(err=> console.log(err.message))
+    
+  }
+  
+  
+  
+  
+  render() {
+    const { userData, followers } = this.state
+    const { login } = userData
+    console.log(userData, followers)
+    console.log({login})
+
+    return (
+      <div>
+        <h1>Class Components</h1>
+        <h2>Learning The Basics</h2>
+        <h3>With My Friends</h3>
+        <div className="header">
+
+        </div>
+        <div className="container">
+        <Card 
+        {...userData} 
+        />
+        
+        {followers.map((data)=> (
+            <Card {...data}/>
+        ))}
+        
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ github-card/src/components/Card.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Card extends Component {
+    render() {
+        return (
+            <div className='card'>
+                <img className="Img" src={this.props.avatar_url} alt="user"/>
+                <div className='card-info'>
+                <h1>{this.props.login}</h1>
+                <p className='username'> Username: {this.props.login} </p>
+                <p>Location: {this.props.location}</p>
+                
+                <p>Followers: {this.props.followers}</p>
+                <p>Following:{this.props.following}</p>
+                <p>Bio:{this.props.bio}</p>
+            
+            </div>
+            </div>
+        )
+    }
+}
+
+export default Card
